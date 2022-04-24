6ee4b4d15bf256784b0238d0f09d56b6676b2543
Jey Bloody <bloodjey98@gmail.com>
2022-04-24 14:47:41 +0500

    Update: Jump changes

--- Assets/Scripts/Character/CharacterJump.cs
+++ Assets/Scripts/Character/CharacterJump.cs
@@ -6,39 +6,59 @@ namespace Character
     public sealed class CharacterJump : IJump
     {
         private readonly float _jumpForce;
+        private readonly float _jumpTime;
         private readonly Rigidbody2D _characterRigidBody;
         private readonly Animator _characterAnimator;
         private static readonly int IsJump = Animator.StringToHash("IsJump");
 
         private const float VelocityMultiplier = .5f;
         private const string JumpButton = "Jump";
+        private bool _isJumping;
+        private float _jumpTimeCounter;
 
-        public CharacterJump(float jumpForce, Rigidbody2D characterRigidBody, Animator characterAnimator)
+
+        public CharacterJump(float jumpForce, float jumpTime, Rigidbody2D characterRigidBody,
+            Animator characterAnimator)
         {
             _jumpForce = jumpForce;
+            _jumpTime = jumpTime;
             _characterRigidBody = characterRigidBody;
             _characterAnimator = characterAnimator;
         }
 
+
         public void Jump(bool isGrounded)
         {
-            bool isJumpPressed = Input.GetButtonDown(JumpButton);
             if (isGrounded)
             {
                 _characterAnimator.SetBool(IsJump, false);
             }
 
-            if (isJumpPressed && isGrounded)
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
             {
-                _characterRigidBody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+                // _characterRigidBody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+                _isJumping = true;
+                _jumpTimeCounter = _jumpTime;
+                _characterRigidBody.velocity = Vector2.up * _jumpForce;
                 _characterAnimator.SetBool(IsJump, true);
             }
 
-            if (isJumpPressed && _characterRigidBody.velocity.y > 0)
+            if (Input.GetKey(KeyCode.Space) && _isJumping)
             {
-                _characterRigidBody.velocity =
-                    new Vector2(_characterRigidBody.velocity.x, _characterRigidBody.velocity.y * VelocityMultiplier);
-                _characterAnimator.SetBool(IsJump, true);
+                if (_jumpTimeCounter > 0)
+                {
+                    _characterRigidBody.velocity = Vector2.up * _jumpForce;
+                    _jumpTimeCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    _isJumping = false;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                _isJumping = false;
             }
         }
     }
