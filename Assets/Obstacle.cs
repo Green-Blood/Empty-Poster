bc715177723aa30286fba5f3b9621b25d00bd8af
Th3Dilli <th3dilli@gmx.at>
2022-04-24 13:29:41 +0200

    add particls for destruction of obstacles

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    private ParticleSystem destory;
+    private bool IsHidden = false;
+    public GameObject gameObject1;
+
+    public void Start()
+    {
+        destory = GetComponent<ParticleSystem>();
+        // gameObject1 = GetComponentInChildren<GameObject>();
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.TryGetComponent(out EnemyAI enemy)) return;
-        gameObject.SetActive(false);
+        Hide();
     }
+
+    public void Hide()
+    {
+        if (!IsHidden)
+        {
+            IsHidden = true;
+            destory.Play();
+            gameObject1.SetActive(false);
+        }
+    }
+
+    public void Show()
+    {
+        IsHidden = false;
+        gameObject1.SetActive(true);
+    }
+
+
 }
